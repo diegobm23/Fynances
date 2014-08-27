31151a77a8f7b40b2adc00b906b236ef87c64db8
diegobm23 <diegomoreira@MacBook-Diego.local>
2014-08-27 00:49:25 -0300

    First prototype in the storyboard

--- Fynances/AppDelegate.h
+++ Fynances/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  Fynances
 //
-//  Created by Diego Moreira on 27/08/14.
+//  Created by Diego Moreira on 26/08/14.
 //  Copyright (c) 2014 Diego Moreira. All rights reserved.
 //
 
