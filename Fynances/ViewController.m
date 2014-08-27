ddd5e78564486319ab8a987295a75a1d02f748d2
diegobm23 <diegomoreira@MacBook-Diego.local>
2014-08-27 00:43:23 -0300

    Fynances
    First prototype

---
+++ Fynances/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Fynances
+//
+//  Created by Diego Moreira on 27/08/14.
+//  Copyright (c) 2014 Diego Moreira. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
