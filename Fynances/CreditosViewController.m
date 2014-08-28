937c44d0e65bca6647e3e440ee9ef5c08c4fdae6
diegobm23 <diegomoreira@macbook-diego.home>
2014-08-27 23:26:15 -0300

    Add controllers to list scenes.

---
+++ Fynances/CreditosViewController.m
@@ -0,0 +1,49 @@
+//
+//  CreditosViewController.m
+//  Fynances
+//
+//  Created by Diego Moreira on 27/08/14.
+//  Copyright (c) 2014 Diego Moreira. All rights reserved.
+//
+
+#import "CreditosViewController.h"
+
+@interface CreditosViewController ()
+
+@end
+
+@implementation CreditosViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Fynances/ListaDebitoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListaDebitoViewController.h
+//  Fynances
+//
+//  Created by Diego Moreira on 27/08/14.
+//  Copyright (c) 2014 Diego Moreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListaDebitoViewController : UIViewController
+
+@end
